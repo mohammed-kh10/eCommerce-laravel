8f14b60888fc5eea98f3f3ba3a3adaf929c8815c
Mohammed Khaled <81472511+mohammed-kh10@users.noreply.github.com>
2021-09-08 11:53:30 +0200

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'admin.admin-dashboard-component' => 'App\\Http\\Livewire\\Admin\\AdminDashboardComponent',
+  'cart-component' => 'App\\Http\\Livewire\\CartComponent',
+  'checkout-component' => 'App\\Http\\Livewire\\CheckoutComponent',
+  'home-component' => 'App\\Http\\Livewire\\HomeComponent',
+  'shop-component' => 'App\\Http\\Livewire\\ShopComponent',
+  'user.user-dashboard-component' => 'App\\Http\\Livewire\\User\\UserDashboardComponent',
+);
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,11 +11,12 @@ const mix = require('laravel-mix');
  |
  */
 
-mix
-    .js('resources/js/frontend/app.js', 'public/js/app.js')
-    .js('resources/js/backend/app.js', 'public/js/app.js')
-    .sass('resources/sass/app.scss', 'public/css')
+mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require('postcss-import'),
+        require('tailwindcss'),
     ]);
 
+if (mix.inProduction()) {
+    mix.version();
+}
